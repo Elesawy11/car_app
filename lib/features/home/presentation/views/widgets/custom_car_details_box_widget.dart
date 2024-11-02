dcaa080ae52e5ea0078dcbb68a550f5419ea1c48
elesawy2 <elesawy928@gmail.com>
2024-11-02 18:27:19 +0200

    finish custom car widget

---
+++ lib/features/home/presentation/views/widgets/custom_car_details_box_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomCarDetailsBoxWidget extends StatelessWidget {
+  const CustomCarDetailsBoxWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * (0.5 / 4.5),
+      height: 60.h,
+      decoration: BoxDecoration(
+        color: const Color(0xffF7F7FD),
+        // color: Colors.red,
+        borderRadius: BorderRadius.circular(
+          12.r,
+        ),
+      ),
+      child: FittedBox(
+        child: Column(
+          children: [
+            SvgPicture.asset(
+              Assets.assetsIconsHomeAd1,
+              width: 12.w,
+              height: 12.h,
+            ),
+            const Text('السعر'),
+            const Text('12,500')
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/custom_car_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/utils/assets.dart';
+import 'custom_car_details_box_widget.dart';
+
+class CustomCarWidget extends StatelessWidget {
+  const CustomCarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 170.h,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: Column(
+              children: [
+                Container(
+                  color: const Color(0xffD5DBE6),
+                  child: const Center(
+                    child: Text(
+                      'جي ام سي | يوكن | الفئة الرابعة',
+                    ),
+                  ),
+                ),
+                Image.asset(
+                  Assets.assetsImagesImage1,
+                  height: 150.h,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  fit: BoxFit.fill,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 2,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.5,
+              // height: 60.h,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CustomCarDetailsBoxWidget(),
+                  CustomCarDetailsBoxWidget(),
+                  CustomCarDetailsBoxWidget(),
+                  CustomCarDetailsBoxWidget(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
